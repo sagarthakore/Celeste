5722906feb39f19a1e8eaaaba200d79b3333f9d0
Sagar <sathakore@attain.com>
2018-07-13 13:06:17 -0400

    first commit

---
+++ Celeste/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Celeste
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css",
+                "~/Content/themes/smoothness/jquery-ui.css",
+                "~/Content/DataTables/css/dataTables.bootstrap.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataTablesExport").Include(
+                "~/Scripts/DataTables/dataTables.buttons.min.js",
+                "~/Scripts/DataTables/jszip.min.js",
+                "~/Scripts/DataTables/pdfmake.min.js",
+                "~/Scripts/DataTables/vfs_fonts.js",
+                "~/Scripts/DataTables/buttons.html5.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataTables").Include(
+                "~/Scripts/DataTables/jquery.dataTables.js",
+                "~/Scripts/DataTables/dataTables.bootstrap.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
+        }
+    }
+}
